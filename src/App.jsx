eab23f229e68a4456cd94d382f68434d8878fcef
ricardo.gonzalez <luzuriagalejandro@gmail.com>
2023-07-19 18:11:56 +0200

    Check App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { Routes, Route, Navigate } from 'react-router-dom'
+import { Routes, Route, Navigate} from 'react-router-dom'
 import Login from './pages/Login'
 import Background from './components/Background/Background'
 import NavBar from './components/NavBar/NavBar'
@@ -20,27 +20,27 @@ function App() {
         <Background />
         <NavBar />
         <main className='layout' id='layout'>
-        <Routes>
-            <Route path="/" element={<Navigate to="/home" />} />
-            <Route path="/login" element={<RegisteredAuth><Login /></RegisteredAuth>} />
-            <Route
-              path="/home"
-              element={<RequireAuth><Home name="Gamer" /></RequireAuth>}
-            />
-            <Route
-              path="/tictactoe"
-              element={<RequireAuth><TicTacToe /></RequireAuth>}
-            />
-            <Route
-              path="/hangman"
-              element={<RequireAuth><Hangman /></RequireAuth>}
-            />
-            <Route
-              path="/sudoku"
-              element={<RequireAuth><Sudoku /></RequireAuth>}
-            />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
+          <Routes>
+              <Route path="/" element={<Navigate to="/home" />} />
+              <Route path="/login" element={<RegisteredAuth><Login /></RegisteredAuth>} />
+              <Route
+                path="/home"
+                element={<RequireAuth><Home name="Gamer" /></RequireAuth>}
+              />
+              <Route
+                path="/tictactoe"
+                element={<RequireAuth><TicTacToe /></RequireAuth>}
+              />
+              <Route
+                path="/hangman"
+                element={<RequireAuth><Hangman /></RequireAuth>}
+              />
+              <Route
+                path="/sudoku"
+                element={<RequireAuth><Sudoku /></RequireAuth>}
+              />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
         </main>
         <Footer />
       </div>
